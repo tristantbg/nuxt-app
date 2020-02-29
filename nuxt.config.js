3189124acc8dde378c3f17c79e1c27428404e984
Tristan Bagot <tristan.bagot@gmail.com>
2020-02-29 15:52:20 +0100

    ready

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'Nuxt App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -18,11 +18,7 @@ export default {
   /*
   ** Customize the progress-bar color
   */
-  loading: {
-    name: 'none',
-    color: '#3B8070',
-    background: 'white'
-  },
+  loading: false,
   /*
   ** Global CSS
   */
